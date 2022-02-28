d37053cc87655f062d83ec4a72411ebf8fe13ebd
michael-muga <mugamichaelshawn@gmail.com>
2022-02-28 15:50:08 +0300

    new template to display the function news

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 #initialising the app
 from flask import Flask, app
-
+from .config import DevConfig
 app = Flask(__name__)
+
+#setting up configurations
+app.config.from_object(DevConfig)
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,7 +1,15 @@
 #creating index page view function
+from ast import excepthandler
 from flask import render_template
 from app import app
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+
+    Title = "Home - Get the latest and Upto date news as they happen from all around the world"
+    return render_template('index.html',title = Title)
+
+@app.route('/news/<int:news_id>')
+def news(news_id):
+
+    return render_template('news.html',id = news_id)
