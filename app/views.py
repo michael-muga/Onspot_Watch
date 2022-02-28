253a953befc2a06f0cce7e59547838244210d250
michael-muga <mugamichaelshawn@gmail.com>
2022-03-01 00:13:09 +0300

    changed the news source

--- app/views.py
+++ app/views.py
@@ -2,12 +2,15 @@
 from ast import excepthandler
 from flask import render_template
 from app import app
+from .request import get_news
 
 @app.route('/')
 def index():
 
+    topHeadlines = get_news('everything')
+    print(topHeadlines)
     Title = "Home - Get the latest and Upto date news as they happen from all around the world"
-    return render_template('index.html',title = Title)
+    return render_template('index.html',title = Title, everything = topHeadlines  )
 
 @app.route('/news/<int:news_id>')
 def news(news_id):
