a8459422df50511a20fcfdc2400ce2f7fcc908b5
michael-muga <mugamichaelshawn@gmail.com>
2022-03-01 00:09:04 +0300

    modification to the instance

--- app/news_test.py
+++ app/news_test.py
@@ -4,9 +4,9 @@ News = news.News
 
 class NewsTest(unittest.TestCase):
     def setUp(self):
-        self.new_news = News(123,'Russia invades Ukrain','The rescent updates on the russians attack on the ukrain soil',
+        self.new_news = News(123,'rusev','Russia invades Ukrain','The rescent updates on the russians attack on the ukrain soil',
         'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.themoscowtimes.com%2F2022%2F02%2F25%2Frussia-attacks-ukraine-as-its-happening-a76553&psig=AOvVaw2ogZrO2fp7kGGBhYF8e8zP&ust=1646141820099000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIiNqajCovYCFQAAAAAdAAAAABAD',
-        8.5,129993)
+        'https://www.themoscowtimes.com/2022/02/27/us-asks-americans-in-russia-to-consider-leaving-now-a76638','Russian President Vladimir Putin has launched a full-scale invasion of Ukraine, forcing residents to flee for their lives and leaving hundreds dead.')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_news,News))
