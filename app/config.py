55c0d62e9942f436fb02f03bc2a95c7b22615bb5
michael-muga <mugamichaelshawn@gmail.com>
2022-03-01 00:06:52 +0300

    api url added

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from distutils.debug import DEBUG
 
 class Config:
     pass
-    NEW_API_URL = 'https://api.newsapi.org/3/news/{}?api_key={}'
+    NEWS_API_URL = 'https://newsapi.org/v2/{}?q=apple&from=2022-02-27&to=2022-02-27&sortBy=popularity&apikey={}'
 
 class ProdConfig(Config):
     pass
